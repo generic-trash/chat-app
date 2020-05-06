dee2e5800a5c011f0c32947dfcfbdf84d1c9fc42
***** *** <rohroexperiment@rohroexperiment.com>
2020-05-06 11:14:27 +0530

    refactor js

--- static/js/Sign-up.js
+++ static/js/Sign-up.js
@@ -1,46 +1,46 @@
 $(function() {
-inputs = $('input').attr('class','')
-password = $('#password')
-username = $('#username')
-cpass = $('#cpassword')
-email = $('#email')
-errors = $('h6').text(null)
-pwerr = $('#pwh6')
-cperr = $('#cph6')
-emerr = $('#emh6')
-userr = $('#ush6')
-$('form').submit(function(e) {
-        e.preventDefault();
-        errors.text(null)
-        inputs.attr('class','')
-        var authxhr = new XMLHttpRequest()
-        body = JSON.stringify({
-        'email':email.val(),
-        'username':username.val(),
-        'password':password.val(),
-        'confirm':cpass.val()
-        })
-        authxhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                resp = JSON.parse(this.responseText)
-                if (resp.status == 'success') {
-                    window.location.href = '/getuser'
-                } else if (resp.csrf == true) {
-                    reloadtoken()
-                } else {
-                    userr.text(resp.errors.username)
-                    pwerr.text(resp.errors.password)
-                    cperr.text(resp.errors.confirm)
-                    emerr.text(resp.errors.email)
-                    if(userr.text()) username.addClass('invalid');
-                    if(pwerr.text()) password.addClass('invalid');
-                    if(emerr.text()) email.addClass('invalid');
-                    if(cperr.text()) cpass.addClass('invalid');
+    inputs = $('input').attr('class','')
+    password = $('#password')
+    username = $('#username')
+    cpass = $('#cpassword')
+    email = $('#email')
+    errors = $('h6').text(null)
+    pwerr = $('#pwh6')
+    cperr = $('#cph6')
+    emerr = $('#emh6')
+    userr = $('#ush6')
+    $('form').submit(function(e) {
+            e.preventDefault();
+            errors.text(null)
+            inputs.attr('class','')
+            var authxhr = new XMLHttpRequest()
+            body = JSON.stringify({
+            'email':email.val(),
+            'username':username.val(),
+            'password':password.val(),
+            'confirm':cpass.val()
+            })
+            authxhr.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    resp = JSON.parse(this.responseText)
+                    if (resp.status == 'success') {
+                        window.location.href = '/getuser'
+                    } else if (resp.csrf == true) {
+                        reloadtoken()
+                    } else {
+                        userr.text(resp.errors.username)
+                        pwerr.text(resp.errors.password)
+                        cperr.text(resp.errors.confirm)
+                        emerr.text(resp.errors.email)
+                        if(userr.text()) username.addClass('invalid');
+                        if(pwerr.text()) password.addClass('invalid');
+                        if(emerr.text()) email.addClass('invalid');
+                        if(cperr.text()) cpass.addClass('invalid');
+                    }
                 }
             }
-        }
-        authxhr.open('POST','/register')
-        authxhr.setRequestHeader('X-CSRF-Token', getcsrftok());
-        authxhr.send(body);
-    })
+            authxhr.open('POST','/register')
+            authxhr.setRequestHeader('X-CSRF-Token', getcsrftok());
+            authxhr.send(body);
+        })
 })
