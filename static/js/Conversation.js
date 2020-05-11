670b4e7bb289364cc78fd974ef964b7918f6b34b
***** *** <rohroexperiment@rohroexperiment.com>
2020-05-11 14:35:55 +0530

    migrate conversation.js to fetch

--- static/js/Conversation.js
+++ static/js/Conversation.js
@@ -21,20 +21,35 @@ function add_conversation_comments(comments) {
     }
 }
 
-function getnewcomments() {
+async function getnewcomments() {
     if (!deferpoll) {
-        var xhrx = new XMLHttpRequest()
-        xhrx.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                    data = JSON.parse(this.responseText)
-                    convolength += data.length
-                    add_conversation_comments(data)
-            } else if (this.readyState == 4 && this.status == 500) {
+            await fetch('/conversations/'+window.location.search.slice(1), {
+                "credentials": "include",
+                "headers": {
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0",
+                    "Accept": "*/*",
+                    "Accept-Language": "en-US,en;q=0.5",
+                    "Content-Type": "text/plain;charset=UTF-8",
+                    "Pragma": "no-cache",
+                    "Cache-Control": "no-cache"
+                },
+                "body": JSON.stringify({"no_of_convos": convolength}),
+                "method": "POLL",
+                "mode": "cors"
+            }).then(function (response) {
+                if (response.status == 500) {
                     window.location.href = '/'
-            }
-        }
-        xhrx.open('POLL','/conversations/'+window.location.search.slice(1))
-        xhrx.send(JSON.stringify({"no_of_convos": convolength}))
+                } else if (response.status != 200) {
+                    return
+                }
+
+                response.json().then(
+                    function (data) {
+                        convolength += data.length
+                        add_conversation_comments(data)
+                    }
+                )
+            });
     } else {
         deferpoll = false;
     }
@@ -82,21 +97,31 @@ $('#sentinel-css').remove()
 initialize()
 convolength = 0
 
-$('form').submit(function(e) {
+$('form').submit(async function(e) {
     e.preventDefault()
     if($('input').val().trim() != "") {
-        var xhr3 = new XMLHttpRequest()
-        xhr3.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                    data = JSON.parse(this.responseText)
-                    convolength += data.length
-                    add_conversation_comments(data)
-            }
-        }
-        xhr3.open('POST','/conversations/'+window.location.search.slice(1))
-        xhr3.send(JSON.stringify({"no_of_convos": convolength,'comment':$('input').val()}))
-        $('input').val("")
-        deferpoll = true;
+          await fetch("http://localhost:5000/conversations/3VQPQM2ZPNAFGQEYPBEZDR3RWZCISX4NT52ECDNETMJS7XLHY2BK3DO7SULW42Q7CX7IFDIJJYEYQKAWHTHSM2LDP3K2YI4R5GEKYK5D", {
+            "credentials": "include",
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0",
+                "Accept": "*/*",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Content-Type": "text/plain;charset=UTF-8"
+            },
+            "body": JSON.stringify({"no_of_convos": convolength,'comment':$('input').val()}),
+            "method": "POST",
+            "mode": "cors"
+            }).then( function(response)  {
+                if (response.status == 200) {
+                    response.json().then(function (data) {
+                        convolength += data.length
+                        add_conversation_comments(data)
+                    })
+                }
+            });
+            deferpoll = true;
     }
+    $('input').val("")
 })
+window.scrollTo(0, document.body.scrollHeight)
 setInterval(getnewcomments, 5000)
