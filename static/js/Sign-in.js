621f6780224f98e5b5aec4fef22f9dfe636f422d
***** *** <rohroexperiment@rohroexperiment.com>
2020-05-14 12:59:17 +0530

    fetch migrate

--- static/js/Sign-in.js
+++ static/js/Sign-in.js
@@ -2,20 +2,22 @@ inputs =  $('input')
 inputs.attr('class', '');
 err = $('h6')
 err.hide();
-$('form').submit(function(e) {
+$('form').submit(async function(e) {
     e.preventDefault()
     inputs.attr('class','')
     err.hide()
-    var authreq = new XMLHttpRequest()
     body = JSON.stringify({'username': $('#username').val(), 'password':$('#password').val()})
-    authreq.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    await fetch("/login", {
+        "credentials": "include",
+        "body": body,
+        "method": "POST",
+        "mode": "cors"
+    }).then(function(response) {
+        if (response.status == 200) {
             window.location = '/'
-        } else if (this.readyState == 4 && this.status == 403) {
+        } else if (response.status == 403) {
             inputs.addClass('invalid')
             err.show()
         }
-    }
-    authreq.open('POST', '/login')
-    authreq.send(body)
+    });
 })
