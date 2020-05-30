35876c2d4dda3e837e07de7f8cde3dbdf6146253
***** *** <rohroexperiment@rohroexperiment.com>
2020-05-30 08:35:06 +0530

    move to pytest

--- test_app.py
+++ test_app.py
@@ -1,28 +1,46 @@
-import unittest
 from json import dumps, loads
+import pytest
 from web import app
 
 
-class Tests(unittest.TestCase):
-    def setUp(self) -> None:
-        self.app = app.test_client()
+@pytest.fixture
+def client():
+    with app.test_client() as app_client:
+        yield app_client
 
-    def test_login(self):
-        self.app.post('/register', data=dumps({'username': 'test', 'email': 'test@example.com',
-                                               'password': 'testpwd2', 'confirm': 'testpwd2'}))
-        self.app.post('/logout')
-        assert self.app.post('/login', data=dumps({'username': 'test', 'password': 'testpwd2'})).status_code == 200
-        assert self.app.post('/login', data=dumps({'username': 'test', 'password': 'testpw2'})).status_code == 403
-        assert self.app.post('/login',
-                             data=dumps({'username': 'test@example.com', 'password': 'testpwd2'})).status_code == 200
 
-    def test_register(self):
-        assert self.app.post('/register', data=dumps({'username': 'tester', 'email': 'tester@example.com',
-                                                      'password': 'testpwd2',
-                                                      'confirm': 'testpwd2'})).status_code == 200
-        assert self.app.post('/register', data=dumps({'username': 'test', 'email': 'test@example.com',
-                                                      'password': 'tpwd2', 'confirm': 'testpw2'})).status_code == 403
+def create_user(client, username, email, pwd):
+    return client.post('/register',
+                       data=dumps({'username': username, 'email': email, 'password': pwd, 'confirm': pwd}))
 
-    def test_darkmode(self):
-        self.app.post('/login', data=dumps({'username': 'test', 'email': 'test@example.com',
-                                            'password': 'testpwd2', 'confirm': 'testpwd2'}))
+
+def login(client, username, pwd):
+    return client.post('/login', data=dumps({'username': username, 'password': pwd}))
+
+
+def test_login(client):
+    create_user(client, 'test_login', 'test_login@example.com', 'password')
+    assert client.post('/login', data=dumps({'username': 'test_login', 'password': 'password'})).status_code == 200
+    assert client.post('/login', data=dumps({'username': 'test_login', 'password': 'testpw2'})).status_code == 403
+    assert client.post('/login',
+                       data=dumps({'username': 'test_login@example.com', 'password': 'password'})).status_code == 200
+
+
+def test_register(client):
+    client.post('/register', data=dumps({'username': 'tester', 'email': 'tester@example.com',
+                                         'password': 'testpwd2',
+                                         'confirm': 'testpwd2'}))
+    assert client.post('/register', data=dumps({'username': 'test', 'email': 'test@example.com',
+                                                'password': 'tpwd2', 'confirm': 'testpw2'})).status_code == 403
+
+
+def test_darkmode(client):
+    create_user(client, 'test_darkmode', 'test_darkmode@example.com', 'password')
+    assert loads(client.post('/darkmode').data)['darkmode'] is True
+    assert loads(client.get('/darkmode').data)['darkmode'] is True
+    assert loads(client.post('/darkmode').data)['darkmode'] is False
+    assert loads(client.get('/darkmode').data)['darkmode'] is False
+
+
+if __name__ == '__main__':
+    pytest.main()
