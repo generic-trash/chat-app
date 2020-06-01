4abf816c552a881659e6e22ead9c8bd33e0c0c87
***** *** <rohroexperiment@rohroexperiment.com>
2020-06-01 14:40:04 +0530

    move to executor;

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,6 +1,7 @@
 from flask import url_for
 from tests.selenium_functions import *
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_register(selenium, live_server):
