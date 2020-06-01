eae7b797902866958d4f74d13cd9c19ed9c79b1c
***** *** <rohroexperiment@rohroexperiment.com>
2020-06-01 17:16:40 +0530

    chrome tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,17 @@ from web import app as application
 @pytest.fixture('session')
 def app():
     return application
+
+
+@pytest.fixture('session')
+def chrome():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.close()
+
+
+@pytest.fixture('session')
+def firefox():
+    driver = webdriver.Firefox()
+    yield driver
+    driver.close()
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -4,5 +4,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_register(selenium, live_server):
-    selenium.get(url_for('signup_html', _external=True))
+def test_register_firefox(firefox, live_server):
+    firefox.get(url_for('signup_html', _external=True))
+
+
+def test_register_chrome(chrome, live_server):
+    chrome.get(url_for('signup_html', _external=True))
