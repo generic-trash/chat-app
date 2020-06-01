8878d1dc1337e89a68375141e26a545f9db523f5
***** *** <rohroexperiment@rohroexperiment.com>
2020-06-01 16:17:32 +0530

    selenium use default fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,3 @@ from web import app as application
 @pytest.fixture('session')
 def app():
     return application
-
-
-@pytest.yield_fixture('session')
-def selenium():
-    driver = webdriver.Firefox()
-    yield driver
-    driver.close()
