c3128999720eaaf4399b5ea031fd7101f90d67df
***** *** <rohroexperiment@rohroexperiment.com>
2020-06-02 12:13:48 +0530

    change interval

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 
 
 def test_register_firefox_4errs(live_server, firefox: webdriver.Firefox):
-    sleep(0.5)
+    sleep(1)
     firefox.get(url_for('signup_html', _external=True))
     em_err = firefox.find_element(By.CSS_SELECTOR, '#emh6')
     us_err = firefox.find_element(By.CSS_SELECTOR, '#ush6')
@@ -24,7 +24,7 @@ def test_register_firefox_4errs(live_server, firefox: webdriver.Firefox):
 
 
 def test_register_firefox_whitespace(live_server, firefox: webdriver.Firefox):
-    sleep(0.5)
+    sleep(1)
     firefox.get(url_for('signup_html', _external=True))
     us_err = firefox.find_element(By.CSS_SELECTOR, '#ush6')
     register(firefox, 'Hello world', 'testing', pwd='pass', conf='conf')
@@ -33,7 +33,7 @@ def test_register_firefox_whitespace(live_server, firefox: webdriver.Firefox):
 
 
 def test_register_firefox_tab(live_server, firefox: webdriver.Firefox):
-    sleep(0.5)
+    sleep(1)
     firefox.get(url_for('signup_html', _external=True))
     us_err = firefox.find_element(By.CSS_SELECTOR, '#ush6')
     register(firefox, 'Hello\tworld', 'testing', pwd='pass', conf='conf')
@@ -42,7 +42,7 @@ def test_register_firefox_tab(live_server, firefox: webdriver.Firefox):
 
 
 def test_register_chrome_4errs(live_server, chrome: webdriver.Chrome):
-    sleep(0.5)
+    sleep(1)
     chrome.get(url_for('signup_html', _external=True))
     em_err = chrome.find_element(By.CSS_SELECTOR, '#emh6')
     us_err = chrome.find_element(By.CSS_SELECTOR, '#ush6')
@@ -57,7 +57,7 @@ def test_register_chrome_4errs(live_server, chrome: webdriver.Chrome):
 
 
 def test_register_chrome_whitespace(live_server, chrome: webdriver.Chrome):
-    sleep(0.5)
+    sleep(1)
     chrome.get(url_for('signup_html', _external=True))
     us_err = chrome.find_element(By.CSS_SELECTOR, '#ush6')
     register(chrome, 'Hello world', 'testing', pwd='pass', conf='conf')
