29ca14fb5399bf2cd50cdec73a5c6dcf33f9007a
***** *** <rohroexperiment@rohroexperiment.com>
2020-06-05 08:05:52 +0530

    remove browser tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,7 @@
 import pytest
-from selenium import webdriver
 from web import app as application
-import urllib3
 
 
 @pytest.fixture
 def app():
     return application
-
-
-@pytest.fixture
-def chrome():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-
-
-@pytest.fixture
-def pool():
-    pool = urllib3.PoolManager()
-    yield pool
-    pool.clear()
-
-
-@pytest.fixture
-def firefox():
-    driver = webdriver.Firefox()
-    yield driver
-    driver.quit()
